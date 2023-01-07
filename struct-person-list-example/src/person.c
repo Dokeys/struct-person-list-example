e4118f52bf4b79fe6b6678d288c9e7eb99af38a2
Dominik <dominikknoll@web.de>
2023-01-08 00:28:32 +0100

    small formatting changes

--- struct-person-list-example/src/person.c
+++ struct-person-list-example/src/person.c
@@ -12,7 +12,7 @@
 #include <stddef.h>
 #include <string.h>
 
-struct person_s{
+struct person_s {
 	char *first_name;
 	char *last_name;
 	unsigned int age;
@@ -32,9 +32,9 @@ person_t* create_person(const char *first_name, const char *last_name, int age) 
 /* person setters */
 void set_person_first_name(person_t *self, const char *first_name) {
 	if (self->first_name == NULL) // for init
-		self->first_name = malloc(sizeof(char)*strlen(first_name) + 1);
+		self->first_name = malloc(sizeof(char) * strlen(first_name) + 1);
 	else
-		self->first_name = realloc(self->first_name, sizeof(char)*strlen(first_name) + 1);
+		self->first_name = realloc(self->first_name, sizeof(char) * strlen(first_name) + 1);
 
 	if (self->first_name == NULL) {
 		printf("unable to allocate memory for first name of %s.\n", first_name);
@@ -45,9 +45,9 @@ void set_person_first_name(person_t *self, const char *first_name) {
 
 void set_person_last_name(person_t *self, const char *last_name) {
 	if (self->last_name == NULL)  // for init
-		self->last_name = malloc(sizeof(char)*sizeof(last_name) + 1);
+		self->last_name = malloc(sizeof(char) * sizeof(last_name) + 1);
 	else
-		self->last_name = realloc(self->last_name, sizeof(char)*sizeof(last_name) + 1);
+		self->last_name = realloc(self->last_name, sizeof(char) * sizeof(last_name) + 1);
 
 	if (self->last_name == NULL) {
 		printf("unable to allocate memory for last name of %s.\n", last_name);
@@ -94,7 +94,7 @@ void destroy_person(person_t *self) {
 	free(self);
 }
 
-size_t get_sizeof_person_t(){
+size_t get_sizeof_person_t() {
 	return sizeof(person_t);
 }
 
